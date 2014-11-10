293fcd79e016993e70677937b6ab0907cf99c6ce
chensq03123 <csq03123@126.com>
2014-11-10 14:39:34 +0800

    drawerlayout_login layout

--- app/src/main/java/com/hustunique/bocp/Activities/MyActivity.java
+++ app/src/main/java/com/hustunique/bocp/Activities/MyActivity.java
@@ -13,7 +13,7 @@ public class MyActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my);
+        setContentView(R.layout.drawerview);
     }
 
 
